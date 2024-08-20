a097ac5193df32da170374938ead445735122977
Murilo <murilooliveira110305@gmail.com>
2024-08-20 20:14:50 -0300

    test

--- app/teste/page.tsx
+++ app/teste/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 
-import React from 'react'
+import React from "react"
 
 const Home = () => {
-  return (
-    <h1 className='text-red-600 mb-4 bg-red-500'>Teste</h1>
-  )
+  return <h1 className="mb-4 bg-red-500 text-red-600">Teste</h1>
 }
 
 export default Home
