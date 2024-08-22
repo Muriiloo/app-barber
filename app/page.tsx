c8752621a2399bf594f64086c233842b5b9db68f
Murilo <murilooliveira110305@gmail.com>
2024-08-22 19:00:11 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,60 @@
-export default function Home() {
-  return <h1>Testeee</h1>
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { CardContent, Card } from "./_components/ui/card"
+import { Input } from "./_components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Murilo</h2>
+        <p>Quinta-feira, 22 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="banner"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia HardCorte</p>
+              </div>
+            </div>
+
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">22</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
+
+export default Home
