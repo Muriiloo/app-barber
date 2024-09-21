e71677325c781d7c9f7402598bc12e00b28ecdfa
Murilo <murilooliveira110305@gmail.com>
2024-09-21 13:29:44 -0300

    feat: ensure bookings are listed on bookings page

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -58,9 +58,11 @@ const Sidebar = () => {
             </Link>
           </Button>
         </SheetClose>
-        <Button className="justify-start gap-2" variant="ghost">
-          <CalendarDaysIcon size={18} />
-          Agendamentos
+        <Button className="justify-start gap-2" variant="ghost" asChild>
+          <Link href="/bookings">
+            <CalendarDaysIcon size={18} />
+            Agendamentos
+          </Link>
         </Button>
       </div>
 
